9383cf79d18fd9814f8d80429a4be43c487adb76
YassMaldini <yass.maldini@gmail.com>
2025-03-09 15:34:08 +0100

    build: upgrade android sdk version to 35

--- app.config.js
+++ app.config.js
@@ -42,9 +42,9 @@ export default {
         'expo-build-properties',
         {
           android: {
-            compileSdkVersion: 34,
-            targetSdkVersion: 34,
-            buildToolsVersion: '34.0.0',
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+            buildToolsVersion: '35.0.0',
           },
         },
       ],
